b53d2e30ffa4a3469438e6df892a4af90d8e7c98
LAPTOP-JJ518A0L\minht <minhthanh5346@gmail.com>
2024-04-04 22:36:53 +0700

    connect to firebase

--- lib/backend/controllers/auth_controller.dart
+++ lib/backend/controllers/auth_controller.dart
@@ -1,49 +1,77 @@
-import 'dart:html';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../consts/firebase_consts.dart';
 import 'package:velocity_x/velocity_x.dart';
-
+import 'package:food_mobile_app/consts/consts.dart';
 class AuthController extends GetxController {
-  Future<UserCredential?> loginMethod({email, password, context}) async {
-    UserCredential? userCredential;
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  User? currentUser = FirebaseAuth.instance.currentUser;
 
+  Future<UserCredential?> loginMethod({String? email, String? password, BuildContext? context}) async {
+    UserCredential? userCredential;
     try {
       userCredential = await auth.signInWithEmailAndPassword(
-          email: email, password: password);
+        email: email!,
+        password: password!,
+      );
     } on FirebaseAuthException catch (e) {
-      VxToast.show(context, msg: e.toString());
+      VxToast.show(context!, msg: e.toString());
     }
     return userCredential;
   }
 
-  Future<UserCredential?> siginupMethod({email, password, context}) async {
+  Future<void> storeUserData({String? name, String? email, String? password}) async {
+    if (currentUser != null && name != null && email != null && password != null) {
+      DocumentReference store =
+      firestore.collection(usersCollection).doc(currentUser!.uid);
+      await store.set({
+        'name': name,
+        'email': email,
+        'password': password,
+        'imageUrl': ''
+      });
+    } else {
+      print("Error storing user data: One or more parameters are null.");
+    }
+  }
+
+  Future<UserCredential?> signupMethod({String? email, String? password, String? name, BuildContext? context}) async {
     UserCredential? userCredential;
-
+    print("Name in controller: ${name}");
+    print("Email in controller: ${email}");
+    print("Password in controller: ${password}");
     try {
+      // Sign up the user with Firebase Authentication
       userCredential = await auth.createUserWithEmailAndPassword(
-          email: email, password: password);
+        email: email!,
+        password: password!,
+      );
+      // If signup successful, store user data in Firestore
+      if (userCredential != null) {
+        await storeUserData(
+          name: name,
+          email: email,
+          password: password,
+        );
+      } else {
+        // Handle case where userCredential is null
+        print("UserCredential is null after sign up.");
+      }
     } on FirebaseAuthException catch (e) {
-      VxToast.show(context, msg: e.toString());
+      // Handle FirebaseAuthException
+      VxToast.show(context!, msg: e.toString());
+    } catch (e) {
+      // Handle other exceptions
+      print("Error during signup: $e");
     }
+
     return userCredential;
   }
 
-  //storing data method
-  storeUserData({name, password, email}) async {
-    DocumentReference store =
-        firestore.collection(usersCollection).doc(currentUser!.uid);
-    store.set({
-      'name': name,
-      'email': email,
-      'password': password,
-      'dateOfBirth': ''
-    });
-  }
 
-  //sign out  method
-  signoutMethod(context) async {
+  Future<void> signoutMethod(BuildContext context) async {
     try {
       await auth.signOut();
     } catch (e) {
--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_mobile_app/backend/controllers/auth_controller.dart';
 import 'package:food_mobile_app/components/buttons.dart';
 import 'package:food_mobile_app/components/custom_textfield.dart';
 import 'package:food_mobile_app/consts/consts.dart';
+import 'package:food_mobile_app/main.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:food_mobile_app/pages/login.dart';
 import 'package:get/get.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
   const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  bool isCheck = false;
+
+  var controller = Get.put(AuthController());
+
+  var nameController = TextEditingController();
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var passwordRetypeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[900], // Setting background color
+      backgroundColor: Colors.blueGrey[900],
       resizeToAvoidBottomInset: false,
       body: Center(
         child: SingleChildScrollView(
-          // Wrap with SingleChildScrollView to avoid overflow
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -29,65 +44,109 @@ class SignupPage extends StatelessWidget {
               SizedBox(height: 15),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
-                // Add horizontal padding
                 child: Column(
                   children: [
                     customeTextField(
                       hint: nameHint,
                       title: name,
+                      controller: nameController,
+                      isPass: false,
                     ),
                     SizedBox(height: 20),
-                    //
                     customeTextField(
                       hint: emailHint,
                       title: email,
+                      controller: emailController,
+                      isPass: false,
                     ),
                     SizedBox(height: 10),
-                    // Add some space between text fields
                     customeTextField(
                       hint: passwordHint,
                       title: password,
+                      controller: passwordController,
+                      isPass: true,
                     ),
                     SizedBox(height: 10),
-                    // Add some space between text fields
                     customeTextField(
                       hint: passwordConfirm,
                       title: password,
+                      controller: passwordRetypeController,
+                      isPass: true,
                     ),
                     SizedBox(height: 20),
-                    // Add more space after text fields
                     Row(
                       children: [
                         Checkbox(
-                            checkColor: Colors.red,
-                            value: false,
-                            onChanged: (newValue) {}),
-                        10.widthBox,
+                          checkColor: Colors.red,
+                          value: isCheck,
+                          onChanged: (newValue) {
+                            setState(() {
+                              isCheck = newValue!;
+                            });
+                          },
+                        ),
+                        SizedBox(width: 10),
                         Expanded(
                           child: RichText(
-                              text: TextSpan(children: [
-                            TextSpan(
-                                text: "I agree to the ",
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                )),
-                            TextSpan(
-                                text: "Terms and Conditions",
-                                style: TextStyle(
-                                  color: Colors.red,
-                                )),
-                          ])),
-                        )
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "I agree to the ",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                                TextSpan(
+                                  text: "Terms and Conditions",
+                                  style: TextStyle(color: Colors.red),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                     SizedBox(height: 10),
-                    // Add more space after text fields
-                    buttons("Sign Up", onPress: () {})
-                        .box
-                        .width(context.screenWidth - 50)
-                        .make(),
+                    buttons(
+                      color: isCheck ? Colors.red : Colors.grey,
+                      title: "Sign Up",
+                      textColor: Colors.white,
+                      onPress: () async {
+                        print("Name: ${nameController.text}");
+                        print("Email: ${emailController.text}");
+                        print("Password: ${passwordController.text}");
+
+                        if (nameController.text.isEmpty ||
+                            emailController.text.isEmpty ||
+                            passwordController.text.isEmpty ||
+                            passwordRetypeController.text.isEmpty) {
+                          VxToast.show(context, msg: "Please fill in all fields.");
+                          return;
+                        }
+                        if (passwordController.text != passwordRetypeController.text) {
+                          VxToast.show(context, msg: "Passwords do not match.");
+                          return;
+                        }
+                        if (!isCheck) {
+                          VxToast.show(context, msg: "Please agree to the Terms and Conditions.");
+                          return;
+                        }
+                        try {
+                          await controller.signupMethod(
+                            context: context,
+                            email: emailController.text,
+                            password: passwordController.text,
+                            name: nameController.text, // Pass the name field here
+                          ).then((value) {
+                            VxToast.show(context, msg: "Logged in successfully");
+                            // Get.offAll(() => HomePage());
+                          });
+                        } catch (e) {
+                          auth.signOut();
+                          print("Error: $e");
+                          // VxToast.show(context, msg: e.toString());
+                        }
+                      },
+                    ).box.width(context.screenWidth - 50).make(),
                     SizedBox(height: 20),
-                    // Add more space after text fields
                     Align(
                       alignment: Alignment.center,
                       child: Column(
@@ -97,9 +156,14 @@ class SignupPage extends StatelessWidget {
                             onPressed: () {},
                             child: "Having an Account ?".text.make(),
                           ),
-                          buttons("Login", onPress: () {
-                            Get.to(() => const LoginPage());
-                          }),
+                          buttons(
+                            title: "Login",
+                            color: Colors.transparent,
+                            textColor: Colors.red,
+                            onPress: () {
+                              Get.to(() => const LoginPage());
+                            },
+                          ),
                         ],
                       ),
                     ),
